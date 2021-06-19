f4280b5f06af19e8dcc0ff28a541cc0206a50f6f
lukeewin <2131672767@qq.com>
2021-06-19 10:58:38 +0800

    second commit

--- src/main/java/top/lukeewin/git/GitDemo.java
+++ src/main/java/top/lukeewin/git/GitDemo.java
@@ -3,5 +3,6 @@ package top.lukeewin.git;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello git !");
+        System.out.println("hello git 2 !");
     }
 }
